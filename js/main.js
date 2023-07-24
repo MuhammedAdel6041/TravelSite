fba81712d91bb02cd23a68dde29f910c05a4692c
[200~mohammedadelbusnis58@gmail.com <mohammedadelbusnis58@gmail.com>
2023-07-24 23:20:16 +0300

    First Commit

--- js/main.js
+++ js/main.js
@@ -1,61 +1,135 @@
-var quote = [
-    {
-        Author: " Oscar Wilde",
-        Quote: "“Be yourself; everyone else is already taken.”",
-        image: "https://images.gr-assets.com/authors/1605640483p8/7715.jpg"
+var siteName = document.getElementById('siteName');
+var siteUrl = document.getElementById('siteUrl');
+var modelBox = document.getElementById("test");
+var warBox = document.getElementById("war");
+var closeBtnwar = document.getElementById("closeElwar");
+var closeBtn = document.getElementById("closeEl");
+var siteGroup = [];
 
-    },
-    {
-        Author: " Albert Einstein",
-        Quote: "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”",
-        image: "https://images.gr-assets.com/authors/1193930952p5/61105.jpg"
-    },
-    {
-        Author: "Pablo Neruda",
-        Quote: "“I love you as certain dark things are to be loved,in secret, between the shadow and the soul.”",
-        image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1651474656i/4026._UX200_CR0,35,200,200_.jpg"
-    },
-    {
-        Author: " Dr. Seuss",
-        Quote: "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.”",
-        image: "https://images.gr-assets.com/authors/1429114964p5/9810.jpg"
-    },
-    {
-        Author: "  Robert Frost",
-        Quote: "“In three words I can sum up everything I've learned about life: it goes on.”",
-        image: "https://images.gr-assets.com/authors/1673611182p5/3565.jpg"
-    },
-    {
-        Author: "  Charles M. Schulz",
-        Quote: "“All you need is love. But a little chocolate now and then doesn't hurt.”",
-        image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1198590750i/209672._UY200_CR51,0,200,200_.jpg"
-    },
-    {
-        Author: "Andre Gide, Autumn Leaves",
-        Quote: "“It is better to be hated for what you are than to be loved for what you are not.”",
-        image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1424119198i/7617._UX200_CR0,29,200,200_.jpg"
-    },
-    {
-        Author: "  Maya Angelou",
-        Quote: "“I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”",
-        image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1379017377i/3503._UX200_CR0,6,200,200_.jpg"
-    },
-    {
-        Author: "  J.K. Rowling",
-        Quote: "“If you want to know what a man's like, take a good look at how he treats his inferiors, not his equals.”",
-        image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1596216614i/1077326._UX200_CR0,15,200,200_.jpg"
-    },
-    {
-        Author: "  Albert Camus",
-        Quote: "“Don’t walk in front of me… I may not followDon’t walk behind me… I may not leadWalk beside me… just be my friend”",
-        image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1686463588i/957894._UY200_CR46,0,200,200_.jpg"
+if (localStorage.getItem("siteList") != null) {
+    siteGroup = JSON.parse(localStorage.getItem("siteList"));
+    displaySiteTable();
+}
+// ################### ADDING SITE #################
+function addSite() {
+    var siteCollection = {
+        name: siteName.value,
+        url: siteUrl.value
     }
 
-]
+    if (siteNameRegex() == true && urlSiteRegex() == true) {
+        if (searchName() != true) {
+            siteGroup.push(siteCollection);
+            localStorage.setItem("siteList", JSON.stringify(siteGroup));
+            displaySiteTable()
+            clearForm();
+        }
+        else{
+            document.getElementById('war').style.display = "flex";
+            clearForm();
+        }
 
-function changeQuote() {
-    var temp = Math.floor(Math.random() * quote.length);
-    document.getElementById('photo').src = quote[temp].image;
-    document.getElementById('Quote').innerHTML = quote[temp].Quote;
-    document.getElementById('Author').innerHTML = quote[temp].Author;
+    }
+    else {
+        document.getElementById('test').style.display = "flex"
+    }
+
+
+}
+// ################### DISPLAY IT INTO TABLE #################
+function displaySiteTable() {
+    var temp = ``
+    for (var count = 0; count < siteGroup.length; count++) {
+
+        temp += ` <tr>
+        <td>`+ (count + 1) + `</td>
+        <td>`+ siteGroup[count].name + `</td>
+        <td> <button onclick="window.open('${siteGroup[count].url}')"   class="btn btn-warning">Visit</button></td>
+        <td><button onclick="deleteSites(`+ count + `)" class="btn btn-danger">Delete</button></td></tr>`
+    }
+    document.getElementById('demo').innerHTML = temp
+}
+// ################### CLEAR FORM AFTER ADDING   #################
+function clearForm() {
+    siteName.value = ""
+    siteUrl.value = ""
+
+}
+// ################### DELETE FROM TABLE SITE #################
+function deleteSites(index) {
+    siteGroup.splice(index, 1)
+    localStorage.setItem("siteList", JSON.stringify(siteGroup));
+    displaySiteTable();
+
+}
+// ################### REGULAR EXPIRATION FUNCTION  #################
+siteName.addEventListener("blur", siteNameRegex)
+
+function siteNameRegex() {
+    var regex = /^[a-z A-Z 0-9]{3,}$/gm;
+    if (regex.test(siteName.value) == true) {
+        siteName.classList.add("is-valid")
+        siteName.classList.remove("is-invalid")
+        return true;
+
+    }
+    else {
+        siteName.classList.add("is-invalid")
+        siteName.classList.remove("is-valid")
+        return false;
+
+    }
+
+
+}
+siteUrl.addEventListener("blur", urlSiteRegex)
+function urlSiteRegex() {
+    var regex = /(https:\/\/)(?:www\.)?[a-z A-Z ]{0,}(.com)?/gm;
+    if (regex.test(siteUrl.value) == true) {
+        siteUrl.classList.add("is-valid")
+        siteUrl.classList.remove("is-invalid")
+        return true;
+
+    }
+    else {
+        siteUrl.classList.add("is-invalid")
+        siteUrl.classList.remove("is-valid")
+        return false;
+
+    }
+
+}
+// ################### CLOSE BUTTON  #################
+
+closeBtn.addEventListener("click", closePhoto)
+function closePhoto() {
+
+    modelBox.style.display = "none";
+  
+}
+
+
+closeBtnwar.addEventListener("click", closePhotoWar)
+function closePhotoWar() {
+
+    warBox.style.display = "none";
+  
+}
+
+// ################### SEARCH THE SAME NAME   #################
+
+function searchName() {
+
+    var site = siteName.value;
+    for (var count = 0; count < siteGroup.length; count++) {
+        if (siteGroup[count].name.includes(site) == true) {
+            console.log(siteGroup[count].name.includes(site))
+           
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
 }
